f2e3ef093332e1148ef1eca929a7dca72b7aa888
Latarho <Latarho2@gmail.com>
2021-01-19 22:57:46 +0300

    Verifying Valid Blockchains

--- blockchain.py
+++ blockchain.py
@@ -27,31 +27,24 @@ class Block():
             self.nonce)
 
     def __str__(self):
-        return str("Block#: %s\nHash: %s\nPrevious:" +
-            "%s\nData: %s\nNonce: %s\n" %(
-            self.number,
-            self.hash(),
-            self.previous_hash,
-            self.data,
-            self.nonce))
+        return str("Block#: %s\nHash: %s\nPrevious hash:" "%s\nData: %s\nNonce: %s\n" %(self.number, self.hash(), self.previous_hash, self.data, self.nonce))
 
 
 class Blockchain():
-    difficulty = 3
+    difficulty = 1
 
     def __init__(self, chain=[]):
         self.chain = chain
 
     def add(self, block):
-        self.chain.append({'hash': block.hash(),
-                           'previous': block.previous_hash,
-                           'number': block.number,
-                           'data': block.data,
-                           'nonce': block.nonce})
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
 
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].get('hash')
+            block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
 
@@ -61,6 +54,14 @@ class Blockchain():
             else:
                 block.nonce += 1
 
+    def isVaild(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0" * self.difficulty:
+                return False
+
+            return True
 
 
 def main():
@@ -74,6 +75,10 @@ def main():
 
     for block in blockchain.chain:
         print(block)
+
+    blockchain.chain[2].data = 'NEW DATA'
+    blockchain.mine(blockchain.chain[2])
+    print(blockchain.isVaild())
 
 
 if __name__ == '__main__':
