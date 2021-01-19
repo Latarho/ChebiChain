a4aa7430362f1e120368f81af1e09c82f7c922f8
Latarho <Latarho2@gmail.com>
2021-01-19 22:31:11 +0300

    Mining Blocks

--- blockchain.py
+++ blockchain.py
@@ -8,6 +8,7 @@ def updatehash(*args):
     h.update(hashing_text.encode('utf-8'))
     return h.hexdigest()
 
+
 class Block():
     data = None
     hash = None
@@ -34,11 +35,46 @@ class Block():
             self.data,
             self.nonce))
 
+
 class Blockchain():
-    difficulty = 4
+    difficulty = 3
 
     def __init__(self, chain=[]):
         self.chain = chain
 
     def add(self, block):
-        self.chain.append({'hash': block.hash(), 'previous': block.previous_hash, 'number': block.number, 'data': block.data, 'nonce': block.nonce})
+        self.chain.append({'hash': block.hash(),
+                           'previous': block.previous_hash,
+                           'number': block.number,
+                           'data': block.data,
+                           'nonce': block.nonce})
+
+    def mine(self, block):
+        try:
+            block.previous_hash = self.chain[-1].get('hash')
+        except IndexError:
+            pass
+
+        while True:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block); break
+            else:
+                block.nonce += 1
+
+
+
+def main():
+    blockchain = Blockchain()
+    database = ['Hello world!', 'Whats up', 'Hello', 'Bye']
+
+    num = 0
+    for data in database:
+        num += 1
+        blockchain.mine(Block(data, num))
+
+    for block in blockchain.chain:
+        print(block)
+
+
+if __name__ == '__main__':
+        main()
